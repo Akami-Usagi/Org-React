4f2e638ab1567b1f0398fb1ccce5088d45dc5b9e
Akami-Usagi <akami.3ds@gmail.com>
2023-11-06 19:57:42 -0500

    actualizacion footer

--- src/componentes/footer/footer.jsx
+++ src/componentes/footer/footer.jsx
@@ -3,18 +3,18 @@ import "./footer.css"
 function Footer(){
     return <footer className='footer' style={{ backgroundImage: "url(/img/footer.png"}}>
         <div className='redes'>
-            <a href='https://www.aluracursos.com/'> 
+            <a href='https://www.facebook.com/Akami.Usagi' target="blank"> 
                 <img src="/img/facebook.png" alt="Facebook" />
             </a>
-            <a href='https://www.aluracursos.com/'>
+            <a href='https://twitter.com/Akami_Usagi' target="blank">
                 <img src="/img/twitter.png" alt='twitter' />
             </a>
-            <a href='https://www.aluracursos.com/'>
+            <a href='https://www.instagram.com/akami_usagi/' target="blank">
                 <img src="/img/instagram.png" alt='instagram' />
             </a>
         </div>
         <img src='/img/Logo.png' alt='org' />
-        <strong>Desarrollado por Alura</strong>
+        <strong>Desarrollado Camilo Arango</strong>
     </footer>
 }
 
